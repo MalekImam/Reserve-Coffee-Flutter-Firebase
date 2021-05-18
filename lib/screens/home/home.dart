79be9c21cabe5925638f2f2d66c5b46b3cae631e
Malek <malekalemam20@gmail.com>
2021-05-18 18:50:39 +0300

    Bottom sheet Widget structure

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -6,35 +6,48 @@ import 'package:brew_crew/services/database.dart';
 import 'package:brew_crew/screens/home/brew_list.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
+  final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    final AuthService _auth = AuthService();
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+              child: Text('Bottom Sheet'),
+            );
+          });
+    }
 
-    return Container(
-      child: StreamProvider<List<Brew>>.value(
-        initialData: null,
-        value: DatabaseService().brews,
-        child: Scaffold(
-          backgroundColor: Colors.indigo[100],
-          appBar: AppBar(
-            elevation: 0.0,
-            title: Text('Brew Crew'),
-            backgroundColor: Colors.indigo[500],
-            actions: <Widget>[
-              TextButton.icon(
-                label: Text('logout'),
-                icon: Icon(Icons.person),
-                style: TextButton.styleFrom(primary: Colors.white),
-                onPressed: () async {
-                  await _auth.signOut();
-                },
-              )
-            ],
-          ),
-          body: BrewList(),
+    return StreamProvider<List<Brew>>.value(
+      initialData: null,
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.indigo[100],
+        appBar: AppBar(
+          elevation: 0.0,
+          title: Text('Brew Crew'),
+          backgroundColor: Colors.indigo[500],
+          actions: <Widget>[
+            TextButton.icon(
+              label: Text('logout'),
+              icon: Icon(Icons.person),
+              style: TextButton.styleFrom(primary: Colors.white),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            ),
+            TextButton.icon(
+              label: Text('settings'),
+              icon: Icon(Icons.settings),
+              onPressed: () => _showSettingsPanel(),
+              style: TextButton.styleFrom(primary: Colors.white),
+            ),
+          ],
         ),
+        body: BrewList(),
       ),
     );
   }
