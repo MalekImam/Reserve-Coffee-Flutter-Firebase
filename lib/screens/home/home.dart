09640200f55b5e74b63972265a44059b381c7345
Malek <malekalemam20@gmail.com>
2021-05-18 16:06:32 +0300

    firestore stream

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,9 @@
-import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/services/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:brew_crew/screens/home/brew_list.dart';
 
 class Home extends StatelessWidget {
   const Home({Key key}) : super(key: key);
@@ -9,23 +13,28 @@ class Home extends StatelessWidget {
     final AuthService _auth = AuthService();
 
     return Container(
-      child: Scaffold(
-        backgroundColor: Colors.indigo[100],
-        appBar: AppBar(
-          elevation: 0.0,
-          centerTitle: true,
-          title: Text('Home'),
-          backgroundColor: Colors.indigo[500],
-          actions: <Widget>[
-            TextButton.icon(
-              label: Text('logout'),
-              icon: Icon(Icons.person),
-              style: TextButton.styleFrom(primary: Colors.white),
-              onPressed: () async {
-                await _auth.signOut();
-              },
-            )
-          ],
+      child: StreamProvider<QuerySnapshot>.value(
+        initialData: null,
+        value: DatabaseService().brews,
+        child: Scaffold(
+          backgroundColor: Colors.indigo[100],
+          appBar: AppBar(
+            elevation: 0.0,
+            centerTitle: true,
+            title: Text('Home'),
+            backgroundColor: Colors.indigo[500],
+            actions: <Widget>[
+              TextButton.icon(
+                label: Text('logout'),
+                icon: Icon(Icons.person),
+                style: TextButton.styleFrom(primary: Colors.white),
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+              )
+            ],
+          ),
+          body: BrewList(),
         ),
       ),
     );
