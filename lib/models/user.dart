8b68a220455974cfe419c40d533fb3b6a53787a9
Malek <malekalemam20@gmail.com>
2021-05-18 22:22:55 +0300

    UserData Model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,5 +1,14 @@
+import 'package:brew_crew/models/brew.dart';
+
 class LocalUser {
   final String uid;
 
   LocalUser({this.uid}); // Name paramerter using { }
+}
+
+class UserData {
+  final String uid;
+  final Brew brew;
+
+  UserData({this.uid, this.brew});
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
@@ -22,14 +23,27 @@ class DatabaseService {
     return brewCollection.snapshots().map(_brewListFromSnapshot);
   }
 
+  // get brew from document snapshot
+  Brew _getBrewFromDoc(DocumentSnapshot doc) {
+    return Brew(
+      name: doc.get('name') ?? '',
+      sugars: doc.get('sugars') ?? '0',
+      strength: doc.get('strength') ?? 0,
+    );
+  }
+
   // brew list from snapshot
   List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
-    return snapshot.docs.map((doc) {
-      return Brew(
-        name: doc.get('name') ?? '',
-        sugars: doc.get('sugars') ?? '0',
-        strength: doc.get('strength') ?? 0,
-      );
-    }).toList();
+    return snapshot.docs.map(_getBrewFromDoc).toList();
+  }
+
+  // get user document stream
+  Stream<UserData> get userData {
+    return brewCollection.doc(uid).snapshots().map(_userDataFromSnapshot);
+  }
+
+  // user_data from snapshot_document
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserData(uid: uid, brew: _getBrewFromDoc(snapshot));
   }
 }
